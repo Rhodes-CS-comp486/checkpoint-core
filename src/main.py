909c2f9c6339debf77e463c25d8c3bc5c39ebdb4
EnkhmandakhGantulga <ganen-25@rhodes.edu>
2025-02-27 16:11:57 -0600

    initial commit for database

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 from typing import Annotated
 from datetime import datetime, timedelta, timezone
-
+from database import SessionLocal, User as DBUser, engine
 from fastapi import Depends, FastAPI, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jwt.exceptions import InvalidTokenError
+from sqlalchemy.orm import Session
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 fake_users_db = {
     "johndoe": {
@@ -25,7 +32,7 @@ fake_users_db = {
 
 class Token(BaseModel):
     access_token: str
-    token_type:str
+    token_type: str
 
 class Item(BaseModel):
     name: str
@@ -88,20 +95,28 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
 
     return {"access_token": user.username, "token_type": "bearer"}
 
-
 @app.get("/users/me")
 async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)],
-):
+    current_user: Annotated[User, Depends(get_current_active_user)]):
     return current_user
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/login/")
-async def create_item(user: User):
-    return {"user": user}
+@app.post("/users/")
+async def create_user(user: User, db: Session = Depends(get_db)):
+    db_user = DBUser(
+        username=user.username,
+        email=user.email,
+        full_name=user.full_name,
+        hashed_password=fake_hash_password(user.hashed_password),
+        admin=user.admin,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return {"user": db_user}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):

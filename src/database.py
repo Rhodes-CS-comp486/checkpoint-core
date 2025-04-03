c5d11b42fe830ad684f7f28802356ff68d9eb146
jkhall-25@outlook.com <jkhall-25@outlook.com>
2025-04-03 00:25:45 -0500

    Sprint 5 Demo

--- src/database.py
+++ src/database.py
@@ -1,4 +1,5 @@
 from sqlmodel import Field, Session, SQLModel, create_engine
+from datetime import datetime, timedelta
 
 f = open('db//password.txt', 'r')
 password = f.readline()
@@ -8,20 +9,34 @@ engine = create_engine("postgresql://postgres:postgres@db:5432/checkpoint-db", e
 SessionLocal = engine.connect()
 
 class User(SQLModel, table=True):
-    __tablename__ = "users"
-    username: str = Field(primary_key=True, index=True)
+    __tablename__ = "User"
+    username: str = Field(primary_key=True, index=True, unique=True)
     email: str = Field(index=True, unique=True)
     full_name: str 
+    user_id: str = Field(unique=True)
     hashed_password: str
     admin: bool = Field(default=False)
 
 class Item(SQLModel, table=True):
-    id: int = Field(primary_key=True, index=True)
+    __tablename__ = "Item"
+    id: int = Field(primary_key=True, index=True, unique=True) #this should ideally be a serial number (unique)
     name: str = Field(index=True)
     description: str = Field()
     model: str = Field()
     availability: bool = Field() # available = true, ow = false
-    status: str = Field()
+    borrow_period_days: timedelta
+    status: str = Field() 
+
+class Borrow(SQLModel, table=True):
+    __tablename__ = "Borrow"
+    class Config: arbitrary_types_allowed=True
+    borrow_id: int = Field(primary_key=True)
+    item_id: int = Field(index=True, foreign_key="Item.id")
+    username: str = Field(index=True, foreign_key="User.username")
+    date_borrowed: datetime = Field()
+    date_returned: datetime | None = Field(default=None)
+    date_due: datetime = Field()
+    active: bool = Field()
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
@@ -29,3 +44,5 @@ def create_db_and_tables():
 def get_session():
     with Session(engine) as session:
         yield session
+
+
